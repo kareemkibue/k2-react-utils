e1df602ce71e617cc9e9c3aaaab17471b78b7433
Kareem Kibue <kareemkibue@gmail.com>
2018-04-16 19:57:36 -0400

    :wrench: Ignore compiled js

--- index.js
+++
@@ -1,7 +0,0 @@
-export { convertPixelsToRem } from './src/fontConverter';
-export { classify } from './src/classify';
-export { connect } from './src/connect';
-export { Content } from './src/Content';
-export { SVG } from './src/Svg';
-export { Parser } from './src/parser';
-//# sourceMappingURL=index.js.map
