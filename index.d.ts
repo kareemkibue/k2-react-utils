5a06bb6c23edf103a07c79dcd11b99e6ae2aa5c8
Kareem Kibue <kareemkibue@gmail.com>
2018-04-16 17:47:25 -0400

    Add convertPixelToRem, connect, SVG definition types

--- index.d.ts
+++ index.d.ts
@@ -6,7 +6,23 @@
  
 import * as React from 'react';
 
+export function classify(classObject: any):string;
+
 export function connect():any;
 
 
-export function classify(classObject: any):string;
+export function convertPixelsToRem(pixelValue: string, baseFontSize?:string):string;
+
+export declare class SVG extends React.Component<any, any> {}
+
+export function Parser():any;
+
+
+
+/* export { convertPixelsToRem } from './src/fontConverter';
+export { classify } from './src/classify';
+export { connect } from './src/connect';
+export { Content } from './src/Content';
+export { SVG } from './src/Svg';
+export { Parser } from './src/parser';
+ */
