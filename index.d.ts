4e1f23a5676e86d0f7df71ea23390baa457e05da
Kareem Kibue <kareemkibue@gmail.com>
2018-04-16 20:47:54 -0400

    :recycle: Cleanup module definition file

--- index.d.ts
+++ index.d.ts
@@ -30,13 +30,3 @@ export declare class Parser{
 export namespace utils{
     
 }
-
-
-
-/* export { convertPixelsToRem } from './src/fontConverter';
-export { classify } from './src/classify';
-export { connect } from './src/connect';
-export { Content } from './src/Content';
-export { SVG } from './src/Svg';
-export { Parser } from './src/parser';
- */
