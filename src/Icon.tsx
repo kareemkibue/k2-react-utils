391ec1a8741949240dc478472a5c997f4e707524
Kareem Kibue <kareemkibue@gmail.com>
2019-03-03 08:29:35 -0500

    🛑 Comment Icon module out - To Deprecate

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,28 +1,29 @@
-import * as React from 'react';
-import styled from 'styled-components';
+// TODO Deprecate Icon
+// import * as React from 'react';
+// import styled from 'styled-components';
 
-interface IIconProps {
-	icon: string;
-	spritePath?: string;
-	className?: string;
-}
+// interface IIconProps {
+// 	icon: string;
+// 	spritePath?: string;
+// 	className?: string;
+// }
 
-/** Stateless Icon Component */
-const IconContainer: React.StatelessComponent<IIconProps> = (props) => {
-	const { className, icon, spritePath = '' } = props;
+// /** Stateless Icon Component */
+// const IconContainer: React.StatelessComponent<IIconProps> = (props) => {
+// 	const { className, icon, spritePath = '' } = props;
 
-	return (
-		<svg className={className}>
-			<use xlinkHref={`${spritePath}#${icon}`} />
-		</svg>
-	);
-};
+// 	return (
+// 		<svg className={className}>
+// 			<use xlinkHref={`${spritePath}#${icon}`} />
+// 		</svg>
+// 	);
+// };
 
-const Icon = styled(IconContainer)`
-	display: inline-block;
-	vertical-align: top;
-	position: absolute;
-	transition: 0.2s ease-in;
-`;
+// const Icon = styled(IconContainer)`
+// 	display: inline-block;
+// 	vertical-align: top;
+// 	position: absolute;
+// 	transition: 0.2s ease-in;
+// `;
 
-export { Icon, IIconProps };
+// export { Icon, IIconProps };
