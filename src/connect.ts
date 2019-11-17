a400497e3e55e8b3f04695e9d92dc2a567841c29
Kareem Kibue <kareemkibue@gmail.com>
2019-11-16 20:14:15 -0500

    Update: Add useBrowserStorage hook

--- src/connect.ts
+++ src/connect.ts
@@ -1,7 +1,8 @@
 import { connect as connectReactRedux } from 'react-redux';
 
 /**
- * React-Redux Connect implementation for Typescript
+ * react-redux connect decorator implementation for Typescript
+ * Suppressses @types/react-redux issue when using connect as a decorator
  */
 const connect = (mapStateToProps: any, mapDispatchToProps?: any, mergeProps?: any, options?: any) => {
     return (target: any) => (connectReactRedux(mapStateToProps, mapDispatchToProps, mergeProps, options)(target) as any);
--- src/parser.ts
+++ src/parser.ts
@@ -1,19 +1,35 @@
-import * as XmlJs from 'xml-js';
+// import * as XmlJs from 'xml-js';
 
 /**
  * A Parser utility that converts XML<=>JSON built by Nashwaan
  * XML-Js Library - https://github.com/nashwaan/xml-js
  */
 
+// import('xml-js')
+//     .then((something) => {
+//         console.log(something.something);
+//     }).catch((e) => { 
+//         console.log(e)
+//     });
 
 const convertXmlToJson = (xmlNode: any): any => {
-    return JSON.parse(
-        XmlJs.xml2json(xmlNode, {
-            compact: true,
-            ignoreComment: true,
-            spaces: 4,
-        })
-    );
+
+    try {
+        var XmlJs = require('xml-js');
+        console.log({ XmlJs })
+
+        return JSON.parse(
+            XmlJs.xml2json(xmlNode, {
+                compact: true,
+                ignoreComment: true,
+                spaces: 4,
+            })
+        );
+
+    } catch (error) {
+        console.log({ error })
+    }
+
 }
 
 export { convertXmlToJson };
