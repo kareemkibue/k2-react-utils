204e6adcad0ee6bfa0168125fb6ca2cda965873b
Kareem Kibue <kareemkibue@gmail.com>
2019-11-18 16:22:09 -0500

    Tests: Update testcov

---
+++ tests/useBrowserStorage.test.tsx
@@ -0,0 +1,12 @@
+// import '@testing-library/jest-dom/extend-expect';
+// import { useBrowserStorage } from '../src/useBrowserStorage';
+
+test('sets value to local storage', () => {
+    // Act
+    // const [key, setKey] = useBrowserStorage('LOCAL', 'key')
+    // setKey('value')
+
+    // Assert    
+    // expect(key).toBeDefined()
+
+});
