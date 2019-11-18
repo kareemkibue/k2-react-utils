c9aa05d42ccc51aa5de806f2074695348651fbc4
Kareem Kibue <kareemkibue@gmail.com>
2019-11-18 13:34:27 -0500

    Chore: Remove unused commented out code

--- src/parser.ts
+++ src/parser.ts
@@ -1,17 +1,7 @@
-// import * as XmlJs from 'xml-js';
-
 /**
  * A Parser utility that converts XML<=>JSON built by Nashwaan
  * XML-Js Library - https://github.com/nashwaan/xml-js
  */
-
-// import('xml-js')
-//     .then((something) => {
-//         console.log(something.something);
-//     }).catch((e) => { 
-//         console.log(e)
-//     });
-
 const convertXmlToJson = (xmlNode: any): any => {
 
     try {
